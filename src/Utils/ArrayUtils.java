5dc2bf494e3eb384b7f9c12f471cad7dec5908f9
eytereschenko <bezstrawnyiVoin@gmail.com>
2018-01-16 23:44:14 +0200

    some commit;

--- src/Utils/ArrayUtils.java
+++ src/Utils/ArrayUtils.java
@@ -12,9 +12,70 @@ public class ArrayUtils {
     }
 
     public static void printArray(int[] array) {
+
+        System.out.print("Yor random array is: ");
+        for (int anArray : array) {
+            System.out.print(anArray + " ");
+        }
+        System.out.println("");
+    }
+
+    public static int findMax(int[] array) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static int findMin(int[] array) {
+        int min = array[0];
         int length = Array.getLength(array);
         for (int i = 0; i < length; i++) {
-            System.out.print(array[i] + " ");
+            if (min > array[i]) {
+                min = array[i];
+            }
         }
+        return min;
+    }
+
+    public static int[] sortArrayByBubble(int[] array) {
+        int buffer;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    buffer = array[i];
+                    array[i] = array[j];
+                    array[j] = buffer;
+                }
+            }
+        }
+        return array;
+    }
+
+    public static int findPlaceOfMax(int[] array) {
+        int number = 0;
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+                number = i;
+            }
+        }
+        return number;
+    }
+
+    public static int findPlaceOfMin(int[] array) {
+        int number = 0;
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+                number = i;
+            }
+        }
+        return number;
     }
 }
