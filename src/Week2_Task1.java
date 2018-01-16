2147d7682e3b7ffc5292dc76fa540c097dd47281
eytereschenko <bezstrawnyiVoin@gmail.com>
2018-01-16 23:03:30 +0200

    some commit;

--- src/Week2_Task1.java
+++ src/Week2_Task1.java
@@ -1,11 +1,13 @@
 import Utils.*;
 
 import java.lang.reflect.Array;
+import Utils.*;
 
 public class Week2_Task1 {
     public static void main(String[] args) {
         int[] array = ArrayUtils.createRandomArray(10);
         ArrayUtils.printArray(array);
-        System.out.println("2");
+        
+
     }
 }
